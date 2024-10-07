7b64f56168be24b69ca5fbc7fb7efc016fa85b9f
Faizan707 <faizanbutt707@gmail.com>
2024-10-07 22:48:45 +0500

    coorect usefetch

--- frontend/src/hooks/useFetchCategories.jsx
+++ frontend/src/hooks/useFetchCategories.jsx
@@ -1,19 +1,15 @@
 import { useState, useEffect } from 'react';
 import { getAxiosInstance } from '../utils/axios';
-import { addCategories } from '../redux/features/categoriesSlice';
-import { useDispatch } from 'react-redux';
 
 function useFetchCategories() {
   const [categoriesData, setCategoriesData] = useState([]);
-  const dispatch = useDispatch(); 
 
   const fetchCategoriesData = async () => {
     try {
       const axiosInstance = await getAxiosInstance(true);
       const response = await axiosInstance.get('/api/categories');
       setCategoriesData(response.data); 
-      dispatch(addCategories(response.data));
-    } catch (error) {
+ } catch (error) {
       console.error('Error fetching categories:', error);
     }
   };
